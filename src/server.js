46d9ac44026643e8f4b8acaaa8c102fe375021b1
oculartechie <code@oculartechie.com>
2017-03-11 16:20:13 -0500

    Initial version

---
+++ src/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World from Docker!!!\n');
+});
+
+app.listen(8080);
+console.log('Running on http://localhost:8080');
